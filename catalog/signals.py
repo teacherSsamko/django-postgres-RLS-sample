dd50064a6d53740f7541c5c985b4d9142be5fd8d
teacherSsamko <ssamko@kakao.com>
2022-05-09 17:57:43 +0900

    handle superuser and staff

--- catalog/signals.py
+++ catalog/signals.py
@@ -5,8 +5,16 @@ from django.dispatch import receiver
 
 @receiver(post_save, sender=User)
 def create_db_user(sender, instance, created, **kwargs):
+    is_superuser = instance.is_superuser
+    is_staff = instance.is_staff
+    user_id = instance.id
     if created:
-        user_id = instance.id
         with connection.cursor() as cursor:
-            cursor.execute(f'CREATE ROLE "{user_id}"')
+            create_role_stmt = f'CREATE ROLE "{user_id}"'
+            create_role_stmt += " WITH SUPERUSER" if is_superuser else ""
+            cursor.execute(create_role_stmt)
             cursor.execute(f'GRANT author TO "{user_id}"')
+    elif is_superuser or is_staff:
+        alter_role_stmt = f'ALTER ROLE "{user_id}" WITH SUPERUSER'
+        with connection.cursor() as cursor:
+            cursor.execute(alter_role_stmt)
