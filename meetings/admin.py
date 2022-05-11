a75519197422cfe2efd51f6e71ad457b7bb919e5
teacherSsamko <ssamko@kakao.com>
2022-05-11 14:37:34 +0900

    add meeting detail onto admin page and web page

--- meetings/admin.py
+++ meetings/admin.py
@@ -1,20 +1,28 @@
 from django.contrib import admin
-from .models import Meeting, Dashboard
+from .models import Meeting, Dashboard, MeetingDetail
 
-class DashboardInline(admin.TabularInline):
-    model = Dashboard
 
 
 @admin.register(Meeting)
 class MeetingAdmin(admin.ModelAdmin):
     list_display = ('uid', 'owner', 'team')
-    inlines = [DashboardInline]
+
+
 
 
 @admin.register(Dashboard)
 class DashboardAdmin(admin.ModelAdmin):
     list_display = ('meeting', 'view_meeting_team')
+    
+    @admin.display(empty_value="??")
+    def view_meeting_team(self, obj):
+        return obj.meeting.team
 
+
+@admin.register(MeetingDetail)
+class MeetingDetailAdmin(admin.ModelAdmin):
+    list_display = ('meeting', 'view_meeting_team')
+    
     @admin.display(empty_value="??")
     def view_meeting_team(self, obj):
         return obj.meeting.team
--- meetings/views.py
+++ meetings/views.py
@@ -4,12 +4,18 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from rules.contrib.views import PermissionRequiredMixin
 
 
-from .models import Meeting, Dashboard
+from .models import Meeting, Dashboard, MeetingDetail
 
 
 class MeetingListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based view for a list of meetings."""
     model = Meeting
+    paginate_by = 10
+
+
+class MeetingDetailListView(LoginRequiredMixin, generic.ListView):
+    """Generic class-based list view for a list of meeting details."""
+    model = MeetingDetail
     paginate_by = 10
 
 
