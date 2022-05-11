6ec4f67ba29a5a021201b471e15e7687c3717313
teacherSsamko <ssamko@kakao.com>
2022-05-11 11:22:29 +0900

    update html templates & admin site for debugging

--- meetings/admin.py
+++ meetings/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from .models import Meeting, Dashboard
 
+class DashboardInline(admin.TabularInline):
+    model = Dashboard
+
+
 @admin.register(Meeting)
 class MeetingAdmin(admin.ModelAdmin):
     list_display = ('uid', 'owner', 'team')
+    inlines = [DashboardInline]
 
 
 @admin.register(Dashboard)
 class DashboardAdmin(admin.ModelAdmin):
-    list_display = ('meeting', )
+    list_display = ('meeting', 'view_meeting_team')
+
+    @admin.display(empty_value="??")
+    def view_meeting_team(self, obj):
+        return obj.meeting.team
