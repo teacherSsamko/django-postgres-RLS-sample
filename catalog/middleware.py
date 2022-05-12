6085da012e519f3401e3aa82fd72de2c48f2034a
teacherSsamko <ssamko@kakao.com>
2022-05-12 13:45:32 +0900

    apply team_role

--- catalog/middleware.py
+++ catalog/middleware.py
@@ -6,9 +6,15 @@ class RlsMiddleware(object):
     
     def __call__(self, request):
         user_id = request.user.id
+        if user_id:
+            team = request.user.groups.first().name.lower()
+        # TODO: according to what ROLE user need, SET ROLE team or user
         with connection.cursor() as cursor:
             if user_id and "accounts" not in request.path and "admin" not in request.path:
-                cursor.execute(f'SET ROLE "{user_id}"')
+                if team:
+                    cursor.execute(f'SET ROLE "{team}"')
+                else:
+                    cursor.execute(f'SET ROLE "{user_id}"')
             else:
                 cursor.execute(f'SET ROLE "postgres"')
         
