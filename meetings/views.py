e280e6c7828760d18648633d94abc886b43d1d10
teacherSsamko <ssamko@kakao.com>
2022-05-13 10:47:31 +0900

    listView by admin / member role

--- meetings/views.py
+++ meetings/views.py
@@ -4,13 +4,22 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from rules.contrib.views import PermissionRequiredMixin
 
 
-from .models import Meeting, Dashboard, MeetingDetail
+from .models import Meeting, Dashboard, MeetingDetail, Role
 
 
 class MeetingListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based view for a list of meetings."""
     model = Meeting
     paginate_by = 10
+
+    def get_queryset(self):
+        user = self.request.user
+        team = user.groups.first()
+        role = Role.objects.filter(user=user) # hit
+        if role and role.first().user_role == Role.Roles.ADMIN.value:
+            return Meeting.objects.filter(team=team)
+        else:
+            return Meeting.objects.filter(owner=user)
 
 
 class MeetingDetailView(PermissionRequiredMixin, generic.DetailView):
@@ -22,7 +31,10 @@ class MeetingDetailListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based list view for a list of meeting details."""
     model = MeetingDetail
     paginate_by = 10
-    queryset = MeetingDetail.objects.filter()
+
+    def get_queryset(self):
+        team = self.request.user.groups.first()
+        return MeetingDetail.objects.filter(meeting__team=team)
 
 
 class DashboardListView(LoginRequiredMixin, generic.ListView):
